0e3295433064f62cbcf2bb083f41612ba8bca872
v.peresunko <v.peresunko@a1qa.com>
2023-03-22 14:08:05 +0200

    test commit.

--- src/test/java/bddtests/tesrunners/TestRunner.java
+++ src/test/java/bddtests/tesrunners/TestRunner.java
@@ -3,6 +3,8 @@ package bddtests.tesrunners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
+//test line
+
 @CucumberOptions(
         features = {"src/test/java/bddtests/features"},
         glue = {
